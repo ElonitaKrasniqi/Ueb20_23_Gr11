92054e48052840c1a9949bd607dae0ac52e5eb75
DionisSylejmani <151663963+DionisSylejmani@users.noreply.github.com>
2024-01-04 19:43:54 +0100

    jquerry

--- app.js
+++ app.js
@@ -3,3 +3,20 @@ function animatedImage(){
     image.style.opacity=1
 }
 window.addEventListener('load' , animatedImage);
+
+$('#costum_link').hover(
+    function(){
+        $(this).css({
+            "background-color": "#c58f59",
+            "color": "white",
+            "transition": "background-color 0.3s, color 0.3s"
+        });
+    },
+    function(){
+        $(this).css({
+            "background-color": "",
+            "color": "",
+            "transition": "background-color 0.3s, color 0.3s"
+        });
+    }
+);
